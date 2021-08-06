5bfcbb477d9ea5331be9e6a117f76d1a94944c9c
Alison Turner <alisonmoons@gmail.com>
2021-08-06 20:26:55 -0300

    OnCreateDamageInfoPower now takes a card param that created the damage info, though it can be null if something else created it

--- IconsAddon/src/main/java/IconsAddon/powers/OnCreateDamageInfoPower.java
+++ IconsAddon/src/main/java/IconsAddon/powers/OnCreateDamageInfoPower.java
@@ -1,7 +1,8 @@
 package IconsAddon.powers;
 
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 
 public interface OnCreateDamageInfoPower {
-    void onCreateDamageInfo(DamageInfo info);
+    void onCreateDamageInfo(DamageInfo info, AbstractCard instigatorCard);
 }
